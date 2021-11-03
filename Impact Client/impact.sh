b5a9ac667e123c10ec695e3be29856c01a3b9b0e
AnVeid <92579497+AnVeid@users.noreply.github.com>
2021-11-03 17:32:12 +0900

    Add files via upload

---
+++ Impact Client/impact.sh
@@ -0,0 +1,43 @@
+echo ㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁ
+echo 　
+echo Impact Client 설치
+echo 　
+echo ㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁ
+sleep 1
+clear
+echo ㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁ
+echo 　
+echo Impact Client 파일을 다운로드 하는 중입니다.
+echo 　
+echo 창을 절대 닫지 마세요!
+echo 　
+echo ㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁ
+sleep 2
+wget https://github.com/BAN-NO77/Minecraft-Client-Installer/raw/main/Impact%20Client/ImpactInstaller-0.9.5.jar
+clear
+echo ㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁ
+echo 　
+echo  다운로드 완료!
+echo 　
+echo ㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁ
+sleep 2
+clear
+echo ㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁ
+echo 　
+echo  Impact Client 설치 중입니다.
+echo 　
+echo  창을 절대 닫지 마세요!
+echo 　
+echo ㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁ
+sleep 1
+java -jar ImpactInstaller-0.9.5.jar
+rm ImpactInstaller-0.9.5.jar
+clear
+echo ㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁ
+echo 　
+echo  설치 완료! 해당 버전은 2b2t와 같은 무법서버에서만 사용해주세요.
+echo 　
+echo ㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁ
+sleep 2
+clear
+exit
