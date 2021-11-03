04991f8dccb5c3b17383258f2df69e4ebd467694
AnVeid <92579497+AnVeid@users.noreply.github.com>
2021-11-03 17:31:54 +0900

    fabric.sh

---
+++ Fabric/fabric.sh
@@ -0,0 +1,43 @@
+echo ㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁ
+echo 　
+echo Fabric 설치
+echo 　
+echo ㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁ
+sleep 1
+clear
+echo ㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁ
+echo 　
+echo Fabric 파일을 다운로드 하는 중입니다.
+echo 　
+echo 창을 절대 닫지 마세요!
+echo 　
+echo ㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁ
+sleep 2
+wget https://github.com/BAN-NO77/Minecraft-Client-Installer/raw/main/Fabric/fabric-installer-0.8.3.jar
+clear
+echo ㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁ
+echo 　
+echo  다운로드 완료!
+echo 　
+echo ㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁ
+sleep 2
+clear
+echo ㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁ
+echo 　
+echo  Fabric 설치 중입니다.
+echo 　
+echo  창을 절대 닫지 마세요!
+echo 　
+echo ㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁ
+sleep 1
+java -jar fabric-installer-0.8.3.jar
+rm fabric-installer-0.8.3.jar
+clear
+echo ㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁ
+echo 　
+echo  설치 완료!
+echo 　
+echo ㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁ
+sleep 2
+clear
+exit
